13b346824432f98e010fd26da495c992bd6cff70
Snowy <Snowy1013@163.com>
2016-02-26 17:41:14 +0800

    资讯界面PullToRefreshListView完成

---
+++ app/src/main/java/com/snowy/babycare/adapter/InfoListViewAdapter.java
@@ -0,0 +1,73 @@
+package com.snowy.babycare.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.snowy.babycare.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by snowy on 16/2/26.
+ */
+public class InfoListViewAdapter extends BaseAdapter {
+    private Context context;
+    private List<Map<String, Object>> list;
+    public InfoListViewAdapter(Context context, List<Map<String, Object>> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if(convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_lv_info, null);
+            holder.img = (ImageView) convertView.findViewById(R.id.img_item_lv_info);
+            holder.title = (TextView) convertView.findViewById(R.id.title_item_info);
+            holder.date = (TextView) convertView.findViewById(R.id.date_item_info);
+            holder.talk = (ImageView) convertView.findViewById(R.id.btn_talk_item_info);
+            holder.talknum = (TextView) convertView.findViewById(R.id.talk_num_info);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.img.setImageResource((Integer) list.get(position).get("imgId"));
+        holder.title.setText((String)list.get(position).get("title"));
+        holder.date.setText((String)list.get(position).get("date"));
+        holder.talknum.setText((String)list.get(position).get("talknum"));
+        holder.talk.setTag(position);
+
+        return convertView;
+    }
+
+    class ViewHolder  {
+        private ImageView img;
+        private TextView title;
+        private TextView date;
+        private TextView talknum;
+        private ImageView talk;
+    }
+}
--- app/src/main/java/com/snowy/babycare/fragment/InfoContentFragment.java
+++ app/src/main/java/com/snowy/babycare/fragment/InfoContentFragment.java
@@ -1,5 +1,6 @@
 package com.snowy.babycare.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,20 +10,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import com.handmark.pulltorefresh.library.ILoadingLayout;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.snowy.babycare.R;
+import com.snowy.babycare.adapter.InfoListViewAdapter;
 import com.snowy.babycare.adapter.InfoViewPagerAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Snowy on 16/1/20.
  */
-public class InfoContentFragment extends Fragment implements View.OnClickListener{
+public class InfoContentFragment extends Fragment implements View.OnClickListener {
 
-    private ViewPager vp_img_info;
     private LinearLayout tipsBox_info;//存放提示点的容器
     private ImageView[] tips;//提示性点点数组
     private int[] images;//图片ID数组
-    private int currentPage=0;//当前展示的页码
+    private int currentPage = 0;//当前展示的页码
+    private PullToRefreshListView lv_info;
+    private List<Map<String, Object>> infoList;//ListView中展示的数据集合
+    private View viewpager_info;
+    private ViewPager vp_img_info;
+    private InfoListViewAdapter infoListViewAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,20 +48,105 @@ public class InfoContentFragment extends Fragment implements View.OnClickListene
 
         //创建一个view，并返回
         View view = inflater.inflate(R.layout.frag_content_info, container, false);
-        initView(view);//初始化界面
-        initData(); // 初始化数据
+        initViewPager(); // 初始化ViewPager
+        initListView(view);//初始化ListView,将ViewPager添加到ListView的addHeaderView()
 
         return view;
     }
 
-    public void initView(View view){
+    public void initListView(View view) {
         //查找控件
-        vp_img_info = (ViewPager) view.findViewById(R.id.vp_img_info);
-        tipsBox_info = (LinearLayout) view.findViewById(R.id.tipsBox_info);
+        lv_info = (PullToRefreshListView) view.findViewById(R.id.lv_info);
 
+        //初始化ListView
+        infoList = new ArrayList();
+        for (int i = 0; i < 15; i++) {
+            Map<String, Object> map = new HashMap();
+            map.put("imgId", R.drawable.img4);
+            map.put("title", "我是标题党");
+            Date currentDate = new Date();
+            SimpleDateFormat formater = new SimpleDateFormat("MM月dd日");
+            map.put("date", formater.format(currentDate));
+            map.put("talknum", i + "");
+            infoList.add(map);
+        }
+
+        infoListViewAdapter = new InfoListViewAdapter(getActivity(), infoList);
+        lv_info.setAdapter(null);
+        lv_info.getRefreshableView().addHeaderView(viewpager_info);
+        lv_info.setAdapter(infoListViewAdapter);
+
+        //设置上拉还是下拉刷新
+        /**
+         *如果Mode设置成Mode.BOTH，需要设置刷新Listener为OnRefreshListener2，
+         * 并实现onPullDownToRefresh()、onPullUpToRefresh()两个方法。
+         *如果Mode设置成Mode.PULL_FROM_START或Mode.PULL_FROM_END，
+         * 需要设置刷新Listener为OnRefreshListener，同时实现onRefresh()方法。
+         *当然也可以设置为OnRefreshListener2，
+         * 但是Mode.PULL_FROM_START的时候只调用onPullDownToRefresh()方法，
+         * Mode.PULL_FROM_END的时候只调用onPullUpToRefresh()方法.
+         */
+        lv_info.setMode(PullToRefreshBase.Mode.BOTH);
+        //设置上拉下拉时的显示
+        initPull();
+        //设置监听事件
+        lv_info.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                //下拉时重置数据
+                infoList.clear();
+                for (int i = 0; i < 16; i++) {
+                    Map<String, Object> map = new HashMap();
+                    map.put("imgId", R.drawable.img1);
+                    map.put("title", "我是标题党");
+                    Date currentDate = new Date();
+                    SimpleDateFormat formater = new SimpleDateFormat("MM月dd日");
+                    map.put("date", formater.format(currentDate));
+                    map.put("talknum", i + 100 + "");
+                    infoList.add(map);
+                }
+                new FinishRefresh().execute();
+                infoListViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                //上拉时添加数据
+                Map<String, Object> map = new HashMap();
+                map.put("imgId", R.drawable.img3);
+                map.put("title", "我是标题党");
+                Date currentDate = new Date();
+                SimpleDateFormat formater = new SimpleDateFormat("MM月dd日");
+                map.put("date", formater.format(currentDate));
+                map.put("talknum", 100 + "");
+                infoList.add(map);
+                new FinishRefresh().execute();
+                infoListViewAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
-    public void initData() {
+    private void initPull() {
+        ILoadingLayout startLables = lv_info.getLoadingLayoutProxy(true, false);//前者为true表示下拉
+        startLables.setPullLabel("下拉刷新......");//刚下拉是显示
+        startLables.setRefreshingLabel("正在载入.....");//刷新时显示
+        startLables.setReleaseLabel("放开刷新......");//下来一定距离时显示
+
+        ILoadingLayout endLables = lv_info.getLoadingLayoutProxy(false, true);//后者为true表示上拉
+        endLables.setPullLabel("上拉刷新......");
+        endLables.setRefreshingLabel("正在载入.....");
+        endLables.setReleaseLabel("放开刷新......");
+    }
+
+    /**
+     * 初始化ViewPager，以便于添加到listview的header
+     */
+    public void initViewPager() {
+        viewpager_info = LayoutInflater.from(getActivity()).inflate(R.layout.viewpager_info, null);
+        //查找控件
+        tipsBox_info = (LinearLayout) viewpager_info.findViewById(R.id.tipsBox_info);
+        vp_img_info = (ViewPager) viewpager_info.findViewById(R.id.vp_img_info);
+
         //初始化ImagView的id数组
         images = new int[]{R.drawable.img1, R.drawable.img2,
                 R.drawable.img3, R.drawable.img4, R.drawable.img5};
@@ -77,19 +180,21 @@ public class InfoContentFragment extends Fragment implements View.OnClickListene
          *
          */
         int n = Integer.MAX_VALUE / 2 % images.length;
-        int initPosition = Integer.MAX_VALUE / 2 - n ;
+        int initPosition = Integer.MAX_VALUE / 2 - n;
         vp_img_info.setCurrentItem(initPosition);
+
     }
 
+    //初始化viewpager的指示点
     public void initTips() {
         tips = new ImageView[5];
-        for(int i=0; i<tips.length; i++){
+        for (int i = 0; i < tips.length; i++) {
             ImageView imageView = new ImageView(getActivity());
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(10,10));
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(10, 10));
             tips[i] = imageView;
-            if(i==0){
+            if (i == 0) {
                 imageView.setBackgroundResource(R.drawable.page_now_tip);//默认显示第一张图片，第一个点为选中状态
-            }else {
+            } else {
                 imageView.setBackgroundResource(R.drawable.page_tip);
             }
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
@@ -104,6 +209,23 @@ public class InfoContentFragment extends Fragment implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+        }
+    }
+
+    private class FinishRefresh extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            lv_info.onRefreshComplete(); // 设置刷新完成
         }
     }
 }
